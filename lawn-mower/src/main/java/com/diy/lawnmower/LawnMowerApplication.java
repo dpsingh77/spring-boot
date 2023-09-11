97acd95366bd6940ac95a93b7249d0011c796042
dpsingh77 <iuseitnow@gmail.com>
2023-09-10 21:22:32 -0400

    Tweaking template app

--- lawn-mower/src/main/java/com/diy/lawnmower/LawnMowerApplication.java
+++ lawn-mower/src/main/java/com/diy/lawnmower/LawnMowerApplication.java
@@ -2,10 +2,18 @@ package com.diy.lawnmower;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LawnMowerApplication {
 
+    @GetMapping("/")
+    public String home()
+    {
+        return "Welcome to first spring boot!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(LawnMowerApplication.class, args);
     }
