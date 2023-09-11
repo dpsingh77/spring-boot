06736890ab4985288d8e932baac14623bd4d8eae
dpsingh77 <iuseitnow@gmail.com>
2023-09-10 21:06:23 -0400

    Initial spring boot project checkin

---
+++ lawn-mower/src/test/java/com/diy/lawnmower/LawnMowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diy.lawnmower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawnMowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
